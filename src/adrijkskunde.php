8a1b07b59b132f70d1a3fd8ac45573b3c30f4bb8
Thijs Heerze <thijsheerze@gmail.com>
2024-04-03 13:10:12 +0200

    Geschiedenis + Adrijkskunde page done

--- src/adrijkskunde.php
+++ src/adrijkskunde.php
@@ -9,5 +9,81 @@
 <?php 
     include_once('include/header.php');
   ?>
+  <div class=" ">
+    <h1 class="font-bold text-white flex justify-center items-center p-4 py-8 text-xl bg-yellow-800">Water</h1>
+    <div class="flex flex-row space-x-4 p-4 bg-yellow-800">
+        <div class="rounded-xl p-4 w-1/3 shadow-lg ">
+            <h2 class="font-bold text-white">-	Vervuild</h2>
+            <p class="text-white">
+              Het kan zijn dat er in Zuid-Holland watervervuiling is. Belangrijke oorzaken zijn door industriële activiteiten, landbouw, stedelijke 
+              ontwikkeling, scheepvaart, overstromingen en riool overstorten. Deze factoren en andere menselijke activiteiten kunnen bijdragen aan 
+              de vervuiling van water in Zuid-Holland, maar het is belangrijk op te merken dat watervervuiling een complex probleem is dat vaak meerdere
+               oorzaken heeft en dat het verminderen ervan vereist dat verschillende belanghebbenden samenwerken om effectieve oplossingen te vinden.
+            </p>
+        </div>
+
+        <div class="rounded-xl p-4 w-1/3 shadow-lg">
+            <h2 class="font-bold text-white">-	Waterbronnen</h2>
+            <p class="text-white">
+              In Zuid-Holland zijn verschillende waterbronnen te vinden, waaronder rivieren; zoals de Rijn, Maas en de Lek. Kanalen; zoals de Oude Rijn, het Noordhollandsch Kanaal.
+              Meren; zoals de Kagerplassen en de Nieuwkoopse Plassen. Grondwater en de kustlijn van de Noordzee. Deze waterbronnen voorzien niet alleen in drinkwater,
+              maar worden ook gebruikt voor de industrie, landbouw, recreatie en natuurbehoud.
+              Om te leven maken we gebruik van drinkwater, irrigatiewater voor land- en tuinbouw en water voor de natuur. Vaarwater op rivieren, 
+              meren en de zee is nodig voor transport en we hebben water nodig voor de industrie. Voor recreatie hebben we zwem- en vaarwater nodig van een goede kwaliteit.
+            </p>
+        </div>
+
+        <div class="rounded-xl p-4 w-1/3 shadow-lg">
+            <h2 class="font-bold text-white">-	Kanalen </h2>
+            <p class="text-white">
+              Zuid-Holland heeft verschillende kanalen. Je hebt de Nieuwe Waterweg, dit is een belangrijk kanaal dat de haven van Rotterdam verbindt met de Noordzee. 
+              Het is een van de drukst bevaren waterwegen ter wereld en speelt een cruciale rol in de internationale handel. Ook de Oude Rijn is een belangrijk kanaal 
+              dat Leiden met Alphen aan den Rijn verbindt. Het is een historische waterweg die door de eeuwen heen een belangrijke rol heeft gespeeld in de handel en 
+              transport. Ook heb je nog de Delftse Schie, Hollandsche IJssel en de Vliet. Deze kanalen spelen een belangrijke rol in het vervoer van goederen en personen, 
+              de afwatering van het land en bieden mogelijkheden voor recreatie zoals varen, vissen en fietsen langs de oevers. 
+            </p>
+        </div>
+    </div>
+
+    <div class=" ">
+    <div class="flex flex-row space-x-4 p-4">
+        <div class="rounded-xl p-4 w-1/3 shadow-lg ">
+            <h2 class="font-bold">-	Meren </h2>
+            <p class="text-yellow-800">
+              Er zijn verschillende meren in Zuid-Holland. Onder andere; Kagerplassen, dit is een uitgestrekt merengebied tussen Leiden, Sassenheim en Warmond. 
+              Het bestaat uit verschillende meren, waaronder de Kagerplassen zelf, de Warmonder- en de Kaagse Plassen. Brielse Meer, dit meer ligt in de buurt 
+              van Brielle en is ontstaan als gevolg van dijkdoorbraken tijdens de Sint-Elisabethsvloed van 1421. Zevenhuizerplas, dit meer ligt ten oosten van 
+              Rotterdam, in de gemeente Zuidplas. Het is ontstaan door zandwinning voor de aanleg van de wijk Nesselande. Bleiswijkse Zoom, dit is een kunstmatig 
+              meer dat deel uitmaakt van het natuur- en recreatiegebied Rottemeren, gelegen tussen Rotterdam, Bleiswijk en Zoetermeer. Binnenmaas, dit meer ligt 
+              in de Hoeksche Waard en is ontstaan door de afgraving van veen, het omgeven door groene natuur. 
+              Deze meren bieden verschillende mogelijkheden voor watersportactiviteiten, maar ze zijn niet alleen belangrijk voor recreatie, maar spelen ook een rol 
+              in de waterhuishouding en natuurbehoud in de regio. 
+            </p>
+        </div>
+
+        <div class="rounded-xl p-4 w-1/3 shadow-lg">
+            <h2 class="font-bold text-yellow-800">-	Rivieren </h2>
+            <p class="text-yellow-800">
+              Nieuwe Maas: De Nieuwe Maas is een zijrivier van de Maas en stroomt door Rotterdam. Het is een belangrijke waterweg voor scheepvaartverkeer en speelt een centrale rol in de haven van Rotterdam.
+              Oude Maas: De Oude Maas is een andere tak van de Maas en stroomt ten zuiden van Rotterdam. Ook deze rivier is van oudsher belangrijk voor scheepvaart en waterbeheer.
+              Lek: De Lek is een zijrivier van de Neder-Rijn en stroomt langs steden als Nieuwegein, Vianen en Schoonhoven voordat hij in de Nieuwe Maas uitmondt bij Kinderdijk. Het is een belangrijke rivier voor de afvoer van overtollig water uit het Rijngebied.
+              Noord: De rivier de Noord begint bij het dorpje Alblasserdam en stroomt richting het noorden, waar het water uiteindelijk via het Spui en het Haringvliet in de Noordzee terechtkomt.
+              Hollandse IJssel: De Hollandse IJssel is een belangrijke rivier die door Zuid-Holland stroomt en de steden Gouda, Capelle aan den IJssel en Krimpen aan den IJssel verbindt. Het is een historische rivier met een belangrijke rol in de waterhuishouding en recreatie.
+              Deze rivieren spelen een cruciale rol in de waterinfrastructuur van Zuid-Holland en bieden mogelijkheden voor scheepvaart, recreatie en natuurbehoud.
+            </p>
+        </div>
+
+        <div class="rounded-xl p-4 w-1/3 shadow-lg">
+            <h2 class="font-bold text-yellow-800">-	De Nieuwe Maas, haven van Rotterdam </h2>
+            <p class="text-yellow-800">
+              De Nieuwe Maas is een belangrijke rivier in Zuid-Holland, Nederland. Het is een zijrivier van de Maas en speelt een cruciale rol in de waterhuishouding en economie van 
+              de regio. De rivier stroomt door Rotterdam en is een essentieel onderdeel van de haven van Rotterdam, een van de grootste en drukste havens ter wereld. De Nieuwe Maas 
+              verdeelt Rotterdam in het noorden en zuiden en wordt gekenmerkt door zijn brede waterweg en drukke scheepvaartroute. Langs de oevers van de Nieuwe Maas zijn tal van 
+              iconische gebouwen en bezienswaardigheden te vinden, zoals de Erasmusbrug, de Euromast en de skyline van Rotterdam. De rivier biedt ook mogelijkheden voor recreatie, 
+              zoals rondvaarten en watersportactiviteiten.
+            </p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- src/geschiedenis.php
+++ src/geschiedenis.php
@@ -16,42 +16,45 @@
     <img src="./img/tijdlijn.png" alt="Tijdlijn"></div>
 </div>
 
-<div class="flex flex-row space-x-4 p-4 ">
-    <div class="rounded-xl p-4 w-1/3 shadow-lg ">
-        <h2 class="font-bold text-yellow-800">Jacoba van Beieren</h2>
-        <img src="./img/jacoba.png" alt="Jacoba van Beieren">
-        <p class="text-yellow-800">
-            Jacoba van Beieren was geboren op 16 juli 1401 in Zuid-Holland. Ze is 35 jaar geworden. Ze was de enige dochter van Willem van Beieren en zijn tweede vrouw, Margaretha van Bourgondië.
-            Jacoba trouwde twee keer. Haar eerste huwelijk was met Jan van Touraine. Na zijn dood in 1417 trouwde ze met haar neef, Jan IV van Brabant. 
-            Jacoba van Beieren was een belangrijke vrouw in de geschiedenis van Nederland. Ze wilde graag de gravin van Holland, Zeeland en Henegouwen zijn, maar dat was niet makkelijk.
-            Toen haar vader stierf, wilden haar oom en zijn zoon die titels ook. Ze vocht tegen hen in lange oorlogen, die bekend staan als de Hoekse en Kabeljauwse twisten.
-            Later werd ze gedwongen om naar Engeland te gaan, waar ze hulp zocht bij haar oom, de koning. Maar ze werd gevangengenomen door haar vijand, Filips de Goede.
-            Gelukkig kon ze ontsnappen en bleef ze vechten voor haar rechten. Uiteindelijk werd er een compromis bereikt waarbij ze haar titels deelde met Filips de Goede.
-            Ze trok zich daarna terug uit de politiek en leefde in vrede tot haar dood. Jacoba's vastberadenheid en strijd laten zien hoe ingewikkeld de politiek was in die tijd.
-        </p>
-    </div>
+<div class=" ">
+    <h1 class="font-bold text-yellow-800 flex justify-center items-center p-4 text-xl">Personen</h1>
+    <div class="flex flex-row space-x-4 p-4">
+        <div class="rounded-xl p-4 w-1/3 shadow-lg ">
+            <h2 class="font-bold text-yellow-800">Jacoba van Beieren</h2>
+            <img src="./img/jacoba.png" alt="Jacoba van Beieren">
+            <p class="text-yellow-800">
+                Jacoba van Beieren was geboren op 16 juli 1401 in Zuid-Holland. Ze is 35 jaar geworden. Ze was de enige dochter van Willem van Beieren en zijn tweede vrouw, Margaretha van Bourgondië.
+                Jacoba trouwde twee keer. Haar eerste huwelijk was met Jan van Touraine. Na zijn dood in 1417 trouwde ze met haar neef, Jan IV van Brabant. 
+                Jacoba van Beieren was een belangrijke vrouw in de geschiedenis van Nederland. Ze wilde graag de gravin van Holland, Zeeland en Henegouwen zijn, maar dat was niet makkelijk.
+                Toen haar vader stierf, wilden haar oom en zijn zoon die titels ook. Ze vocht tegen hen in lange oorlogen, die bekend staan als de Hoekse en Kabeljauwse twisten.
+                Later werd ze gedwongen om naar Engeland te gaan, waar ze hulp zocht bij haar oom, de koning. Maar ze werd gevangengenomen door haar vijand, Filips de Goede.
+                Gelukkig kon ze ontsnappen en bleef ze vechten voor haar rechten. Uiteindelijk werd er een compromis bereikt waarbij ze haar titels deelde met Filips de Goede.
+                Ze trok zich daarna terug uit de politiek en leefde in vrede tot haar dood. Jacoba's vastberadenheid en strijd laten zien hoe ingewikkeld de politiek was in die tijd.
+            </p>
+        </div>
 
-    <div class="rounded-xl p-4 w-1/3 shadow-lg">
-        <h2 class="font-bold text-yellow-800">Desiderius Erasmus</h2>
-        <img src="./img/desiderius.png" alt="Desiderius Erasmus">
-        <p class="text-yellow-800">
-            Desiderius Erasmus was een Nederlandse humanist en theoloog, geboren in Rotterdam. Hij leefde van 1466 tot 1536. Erasmus werkte en reisde in verschillende delen van Europa. 
-            Hij was een belangrijk figuur in de Renaissance en een invloedrijke denker op het gebied van filosofie en theologie. Het bekendste werk van Desiderius Erasmus van Rotterdam is waarschijnlijk "Lof der Zotheid". 
-            Dit boek werd in 1511 uitgebracht. Het gaat over dingen die gebeuren in de samenleving, vooral binnen de kerk en onder de mensen die de macht hebben. 
-            Het boek werd snel populair en had een enorme invloed op de Europese samenleving. "Lof der Zotheid" blijft tot op de dag van vandaag een belangrijk werk in de literatuur.
-            Er zijn ook instellingen, prijzen en locaties vernoemd naar Desiderius Erasmus zoals; de Erasmus Universiteit Rotterdam, de Erasmusbrug, Erasmus Prize, Erasmus metrolijn en nog veel meer.
-        </p>
-    </div>
+        <div class="rounded-xl p-4 w-1/3 shadow-lg">
+            <h2 class="font-bold text-yellow-800">Desiderius Erasmus</h2>
+            <img src="./img/desiderius.png" alt="Desiderius Erasmus">
+            <p class="text-yellow-800">
+                Desiderius Erasmus was een Nederlandse humanist en theoloog, geboren in Rotterdam. Hij leefde van 1466 tot 1536. Erasmus werkte en reisde in verschillende delen van Europa. 
+                Hij was een belangrijk figuur in de Renaissance en een invloedrijke denker op het gebied van filosofie en theologie. Het bekendste werk van Desiderius Erasmus van Rotterdam is waarschijnlijk "Lof der Zotheid". 
+                Dit boek werd in 1511 uitgebracht. Het gaat over dingen die gebeuren in de samenleving, vooral binnen de kerk en onder de mensen die de macht hebben. 
+                Het boek werd snel populair en had een enorme invloed op de Europese samenleving. "Lof der Zotheid" blijft tot op de dag van vandaag een belangrijk werk in de literatuur.
+                Er zijn ook instellingen, prijzen en locaties vernoemd naar Desiderius Erasmus zoals; de Erasmus Universiteit Rotterdam, de Erasmusbrug, Erasmus Prize, Erasmus metrolijn en nog veel meer.
+            </p>
+        </div>
 
-    <div class="rounded-xl p-4 w-1/3 shadow-lg">
-        <h2 class="font-bold text-yellow-800">Johan van Oldenbarnevelt</h2>
-        <img src="./img/johan.png" alt="Johan van Oldenbarnevelt">
-        <p class="text-yellow-800">
-            Johan van Oldenbarnevelt was geboren in Amersfoort, hij leefde van 1547 tot 1619. Hij heeft een lange tijd gewerkt in Den Haag. Hij was een prominente Nederlandse staatsman tijdens de tachtigjarige oorlog. 
-            Hij diende als raadpensionaris van de staten van Holland en West-Friesland en was een belangrijke onderhandelaar tijdens het twaalfjarig bestand, een periode van wapenstilstand in de oorlog tegen Spanje. 
-            Johan was een grote voorstander van religieuze tolerantie. Hij vond dat iedereen zijn eigen geloof moest kunnen hebben zonder problemen. Zijn ideeën hebben echt geholpen om Nederland te vormen tot de tolerante samenleving die het vandaag is.
-            Ook heeft hij veel bijgedragen aan de economische groei van Nederland door de handel en de scheepvaart te stimuleren.
-        </p>
+        <div class="rounded-xl p-4 w-1/3 shadow-lg">
+            <h2 class="font-bold text-yellow-800">Johan van Oldenbarnevelt</h2>
+            <img src="./img/johan.png" alt="Johan van Oldenbarnevelt">
+            <p class="text-yellow-800">
+                Johan van Oldenbarnevelt was geboren in Amersfoort, hij leefde van 1547 tot 1619. Hij heeft een lange tijd gewerkt in Den Haag. Hij was een prominente Nederlandse staatsman tijdens de tachtigjarige oorlog. 
+                Hij diende als raadpensionaris van de staten van Holland en West-Friesland en was een belangrijke onderhandelaar tijdens het twaalfjarig bestand, een periode van wapenstilstand in de oorlog tegen Spanje. 
+                Johan was een grote voorstander van religieuze tolerantie. Hij vond dat iedereen zijn eigen geloof moest kunnen hebben zonder problemen. Zijn ideeën hebben echt geholpen om Nederland te vormen tot de tolerante samenleving die het vandaag is.
+                Ook heeft hij veel bijgedragen aan de economische groei van Nederland door de handel en de scheepvaart te stimuleren.
+            </p>
+        </div>
     </div>
 </div>
 
@@ -86,12 +89,15 @@
         </div>
 
         <div class="rounded-xl p-4 w-1/3 shadow-lg">
-            <h2 class="font-bold text-white">Johan van Oldenbarnevelt</h2>
+            <h2 class="font-bold text-white">Port of Rotterdam</h2>
             <p class="text-white">
-                Johan van Oldenbarnevelt was geboren in Amersfoort, hij leefde van 1547 tot 1619. Hij heeft een lange tijd gewerkt in Den Haag. Hij was een prominente Nederlandse staatsman tijdens de tachtigjarige oorlog. 
-                Hij diende als raadpensionaris van de staten van Holland en West-Friesland en was een belangrijke onderhandelaar tijdens het twaalfjarig bestand, een periode van wapenstilstand in de oorlog tegen Spanje. 
-                Johan was een grote voorstander van religieuze tolerantie. Hij vond dat iedereen zijn eigen geloof moest kunnen hebben zonder problemen. Zijn ideeën hebben echt geholpen om Nederland te vormen tot de tolerante samenleving die het vandaag is.
-                Ook heeft hij veel bijgedragen aan de economische groei van Nederland door de handel en de scheepvaart te stimuleren.
+                Nederland was in de 17e eeuw een belangrijke zeemacht en handelsnatie. De haven van Rotterdam, ook wel bekend als Port of Rotterdam, 
+                groeide uit tot een van de belangrijkste handelshavens in Europa en de stad profiteerde erg van de wereldwijde handel. De haven speelt 
+                een belangrijke rol in de economie van Nederland en Europa. De haven van Rotterdam verwerkt jaarlijks enorme hoeveelheden vracht, 
+                waaronder containers, ruwe olie, olieproducten, chemicaliën, LNG, landbouwproducten en meer. Het is een van de belangrijkste doorvoerhavens
+                voor goederen die worden verhandeld tussen Europa, Azië en andere delen van de wereld. De haven van Rotterdam neemt een gebied in van 
+                ongeveer 12.500 hectare. Het strekt zich uit over een lengte van ongeveer 40 kilometer, langs de Nieuwe Waterweg die de haven verbindt 
+                met de Noordzee.
             </p>
         </div>
     </div>
